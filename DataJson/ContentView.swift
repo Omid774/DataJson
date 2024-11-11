1758a74391a30a4c41d6f318ce76f3b3b0a67848
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-11-11 14:25:06 +0330

    Initial Commit

---
+++ DataJson/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DataJson
+//
+//  Created by Omid Heydarzadeh on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DataJson/DataJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataJsonApp.swift
+//  DataJson
+//
+//  Created by Omid Heydarzadeh on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DataJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
